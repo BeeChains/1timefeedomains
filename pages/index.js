ed1b248ae53221c57c7f566d2d987c3d9738bf6b
BeeChains <37987346+BeeChains@users.noreply.github.com>
2020-08-05 10:19:58 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const Home = () => (
 
     <main>
       <h1 className="title">
-        Welcome to 1timefeedomains!
+        !!!Welcome to 1timefeedomains!!!
       </h1>
 
       <p className="description">
